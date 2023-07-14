f69cd0eb6a59b3ad9f0291aa34c8176bf58c834e
tionis <git@tionis.dev>
2023-07-14 20:32:13 +0200

    Squashed 'src/deps/termbox_next/' content from commit c3cd43d
    git-subtree-dir: src/deps/termbox_next
    git-subtree-split: c3cd43dad15e0a619cf30d9b72d6060a63449d31

---
+++ src/memstream.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "memstream.h"
+
+void
+memstream_init(struct memstream* s, int fd, void* buffer, size_t len)
+{
+	s->file = fd;
+	s->data = buffer;
+	s->pos = 0;
+	s->capa = len;
+}
+
+void
+memstream_flush(struct memstream* s)
+{
+
+	ssize_t byts = write(s->file, s->data, s->pos);
+  (void)byts;
+
+	s->pos = 0;
+}
+
+void
+memstream_write(struct memstream* s, void* source, size_t len)
+{
+	unsigned char* data = source;
+
+	if (s->pos + len > s->capa) {
+		memstream_flush(s);
+	}
+
+	memcpy(s->data + s->pos, data, len);
+	s->pos += len;
+}
+
+void
+memstream_puts(struct memstream* s, const char* str) {
+	memstream_write(s, (void*) str, strlen(str));
+}
